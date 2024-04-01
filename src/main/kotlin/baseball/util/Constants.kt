08e9f8a5d2684f3f4a2da2e0a8f749ef67f9d8fc
cucumber99 <dlsguq1@gmail.com>
2024-04-01 14:06:05 +0900

    feat: 상수 추가

---
+++ src/main/kotlin/baseball/util/Constants.kt
@@ -0,0 +1,11 @@
+package baseball.util
+
+object Constants {
+    const val START_MESSAGE = "숫자 야구 게임을 시작합니다."
+    const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
+    const val BALL_MESSAGE = "볼"
+    const val STRIKE_MESSAGE = "스트라이크"
+    const val FAIL_MESSAGE = "낫싱"
+    const val SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+}
