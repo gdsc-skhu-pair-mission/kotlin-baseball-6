23f3bab19adb0bf5c5a68042283a9b9ae88f91de
cucumber99 <dlsguq1@gmail.com>
2024-04-03 16:41:47 +0900

    refactor: 출력 메소드 수정
    early return을 사용하여 불필요한 else를 제거합니다.

--- src/main/kotlin/baseball/view/Output.kt
+++ src/main/kotlin/baseball/view/Output.kt
@@ -9,24 +9,31 @@ import baseball.util.Constants.STRIKE_MESSAGE
 import baseball.util.Constants.SUCCESS_MESSAGE
 
 class Output {
-    fun printStartMessage() { //게임 시작 문구 출력
+    fun printStartMessage() {
         println(START_MESSAGE)
     }
 
-    fun printInputMessage() { //숫자 입력 문구 출력
+    fun printInputMessage() {
         print(INPUT_MESSAGE)
     }
 
-    fun printResultMessage(ball: Int, strike: Int) { //매칭 결과 문구 출력
-        when {
-            ball == 0 && strike == 0 -> println(FAIL_MESSAGE)
-            ball > 0 && strike > 0 -> println("${ball}${BALL_MESSAGE} ${strike}${STRIKE_MESSAGE}")
-            ball > 0 -> println("${ball}${BALL_MESSAGE}")
-            else -> println("${strike}${STRIKE_MESSAGE}")
+    fun printResultMessage(ball: Int, strike: Int) {
+        if (ball == 0 && strike == 0) {
+            println(FAIL_MESSAGE)
+            return
         }
+        if (ball > 0 && strike > 0) {
+            println("${ball}${BALL_MESSAGE} ${strike}${STRIKE_MESSAGE}")
+            return
+        }
+        if (ball > 0) {
+            println("${ball}${BALL_MESSAGE}")
+            return
+        }
+        println("${strike}${STRIKE_MESSAGE}")
     }
 
-    fun printEndMessage() { //게임 종료 및 플래그 입력 문구 출력
+    fun printEndMessage() {
         println(SUCCESS_MESSAGE)
         println(RESTART_MESSAGE)
     }
