8a61a644b0d0d2c26004384e10b4dd3b72f81ff9
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-04 14:30:11 +0900

    Exception 메세지 수정

--- src/main/kotlin/baseball/CheckException.kt
+++ src/main/kotlin/baseball/CheckException.kt
@@ -12,15 +12,15 @@ fun requireNonEmpty(input: String) {
 }
 
 fun requireInteger(input: String) {
-    require(input.toIntOrNull() != null) { "숫자를 입력해주세요." }
+    require(input.toIntOrNull() != null) { "정수를 입력해주세요." }
 }
 
 fun requireNoDuplicates(input: String) {
     for (i in input) {
-        require(input.count { it == i } == 1) { "중복된 숫자를 입력하셨습니다. 중복되지 않는 숫자를 입력해주세요." }
+        require(input.count { it == i } == 1) { "중복된 숫자를 입력하였습니다. 중복되지 않는 숫자를 입력해주세요." }
     }
 }
 
 fun requireCorrectNumberOfDigits(input: String) {
-    require(input.length == NUMBER_OF_DIGITS) { "세 자리 숫자를 입력해주세요." }
+    require(input.length == NUMBER_OF_DIGITS) { "규칙에 맞지 않는 숫자를 입력하였습니다. ${NUMBER_OF_DIGITS}자리 숫자를 입력해주세요." }
 }
